c3d5e0b5e1e2e6b8ccce3beb80f9e52c1be27820
Manuel Holtgrewe <manuel.holtgrewe@fu-berlin.de>
2014-10-20 12:19:39 +0200

    Update to report generator.

--- cbpipeline/qc_report.py
+++ cbpipeline/qc_report.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 """Collecting quality reports from new pipeline."""
 
-from __future__ import with_statement
+from __future__ import print_function
+
+__author__ = 'Manuel Holtgrewe <manuel.holtgrewe@charite.de>'
 
 import argparse
+import os
 import pkg_resources
 import sys
+import zipfile
 
 import jinja2
-
 
 class AlignmentStatsFile(object):
     """Information stored in a .sta file.
 
+    @ivar file_name         path to .sta file
     @ivar bed               path to BED file for statistics
     @ivar reads             number of reads
     @ivar readsAligned      number of aligned reads
@@ -26,6 +30,7 @@ class AlignmentStatsFile(object):
     """
 
     def __init__(self):
+        self.file_name = None
         self.bed = None
         self.reads = None
         self.readsAligned = None
@@ -37,6 +42,10 @@ class AlignmentStatsFile(object):
         self.totalBedLength = None
 
     @property
+    def alignedRate(self):
+        return 1.0 * self.readsAligned / self.reads
+
+    @property
     def meanCoverage(self):
         if not self.totalBedLength or not self.mappedNucleotides:
             return 0.0
@@ -46,6 +55,7 @@ class AlignmentStatsFile(object):
     def parse(self, f):
         """Parse statistics from given file."""
         # first line is path to BED file
+        self.file_name = f.name
         line = f.readline().strip()
         if not line.startswith('#'):
             raise Exception('First line of .sta file must start '
@@ -145,12 +155,201 @@ def totalBedIntervalLength(path):
     return result
 
 
+class FastQCReport(object):
+    """QC report key metrics.
+
+    Valid keys are:
+
+    basic_statistics, per_base_sequence_quality, per_tile_sequence_quality,
+    per_sequence_quality_scores, per_base_sequence_content, per_sequence_gc_content,
+    per_base_n_content, sequence_length_distribution, sequence_duplication_levels,
+    overrepresented_sequences, adapter_content, kmer_content
+
+    @ivar summary dict mapping the statistics names to "pass", "warn",
+                  "fail", see above for valid keys
+    """
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.summary = {}
+
+    def parse(self, report_str):
+        """Parse from string with report."""
+        for line in report_str.splitlines():
+            if not line.startswith('>>') or line.startswith('>>END_MODULE'):
+                continue
+            line = line[2:]
+            arr = line.split('\t')
+            key = arr[0].lower().replace(' ', '_')
+            value = arr[1].lower()
+            self.summary[key] = value
+
+    @classmethod
+    def loadFromFile(self, path):
+        """Load FastQC report from the ZIP file at path."""
+        # load contents of fastqc_data.txt
+        f = zipfile.ZipFile(path, 'r')
+        for name in f.namelist():
+            if name.endswith('fastqc_data.txt'):
+                fastqc_data = f.read(name)
+                break
+        report = FastQCReport(os.path.basename(path))
+        report.parse(fastqc_data)
+        return report
+
+
+class VcfStatsGenerator(object):
+    """Helper class for building VcfStats objects."""
+
+    def __init__(self, f):
+        self.f = f
+        self.vcf_stats = VcfStats()
+        self.vcf_stats.file_name = f.name
+
+    def run(self):
+        """Perform parsing."""
+        for line in self.f:
+            if line.startswith('#'):
+                continue  # skip header
+            (chrom, pos, id_, ref, alt, qual, filter_,
+             info_, format_, individual) = line.strip().split('\t')[:10]
+
+            # split FORMAT field and get index of genotype
+            fmt = dict([(key, i) for i, key in enumerate(format_.split(':'))])
+            if fmt.get('GT') is None:
+                print('WARNING: no genotype!', file=sys.stderr)
+                continue  # skip records without calls
+            # split INFO field
+            info = set(info_.split(';'))
+            # split call individual
+            call = individual.split(':')
+            # split alternatives
+            alts = alt.split(',')
+
+            # check that is variant
+            gt = self._getGenotypes(ref, fmt, alts, call)
+            if ref == gt[0] and ref == gt[1]:
+                print('Warning: not a variant\n%s\n' % line,
+                      file=sys.stderr)
+                continue
+
+            #print("%s\t%s" % (chrom, pos))
+
+            self._countDbSnp(ref, fmt, info, alts, call)
+            self._countHomHet(ref, fmt, alts, call)
+            self._countVariantKind(ref, fmt, alts, call)
+            self._countSense(ref, fmt, alts, call)
+            self._countTrans(ref, fmt, alts, call)
+        return self.vcf_stats
+
+    def _getGenotypes(self, ref, fmt, alts, calls):
+        """Return actual genotypes"""
+        genotypes = map(int, calls[fmt['GT']].split('/'))
+        #print(genotypes)
+        return [([ref] + alts)[i] for i in genotypes]
+
+    def _countDbSnp(self, ref, fmt, info, alts, calls):
+        """Update db_snp_count, db_snp_rare_counts."""
+        # check that record describes a SNV in our individual
+        genotypes = self._getGenotypes(ref, fmt, alts, calls)
+        min_len = min([len(x) for x in genotypes])
+        max_len = max([len(x) for x in genotypes])
+        if min_len != 1 or max_len != 1:
+            return  # skip, is not a SNV
+        
+        self.vcf_stats.dbsnp_count += int('DB' in info)
+        self.vcf_stats.no_dbsnp_count += int('DB' not in info)
+        # TODO(holtgrew): annotation field for db_snp_rare_counts so far
+
+    def _countHomHet(self, ref, fmt, alts, calls):
+        """Update heterozygous, homozygous counts."""
+        genotypes = self._getGenotypes(ref, fmt, alts, calls)
+        is_hom = (genotypes[0] == genotypes[1])
+        self.vcf_stats.heterozygous += int(not is_hom)
+        self.vcf_stats.homozygous += int(is_hom)
+
+    def _countVariantKind(self, ref, fmt, alts, calls):
+        """Update snv_count, ins_count, del_count."""
+        gts = self._getGenotypes(ref, fmt, alts, calls)
+        for i, gt in enumerate(gts):
+            if i == 1 and gts[0] == gts[1]:
+                continue  # skip calling twice
+            if gt == ref:
+                continue  # ALT not differing from REF
+            if len(gt) == 1 and len(ref) == 1:  # SNP
+                self.vcf_stats.snv_count += 1
+            elif len(gt) > len(ref):  # INSertion
+                self.vcf_stats.ins_count += 1
+            elif len(gt) < len(ref):  # DELetion
+                self.vcf_stats.del_count += 1
+            else:
+                pass  # substitute more than one char, WARN?
+
+    def _countSense(self, ref, fmt, alts, calls):
+        """Update num_missense, num_sense."""
+        # TODO(holtgrew): Add when annotation is there
+
+    def _countTrans(self, ref, fmt, alts, calls):
+        """Update transitions, transversions."""
+        TRANS = set(['GA', 'AG', 'TC', 'CT'])
+        gts = self._getGenotypes(ref, fmt, alts, calls)
+        for i, gt in enumerate(gts):
+            if len(ref) != 1 or len(gt) != 1:
+                continue  # ignore, not SNV
+            if gt == ref:
+                continue  # skip, not variant
+            var = ('%s%s' % (gt, ref)).upper()
+            self.vcf_stats.transitions += int(var in TRANS)
+            self.vcf_stats.transversions += int(var not in TRANS)
+
+
+class VcfStats(object):
+    """Statistics on a VCF file.
+
+    @ivar file_name        str, name of analyzed file
+    @ivar no_dbsnp_count   number of SNVs not in dbSNP
+    @ivar dbsnp_count      number of SNVs in dbSNP
+    @ivar dbsnp_rare_count number of rare variants from dbSNP
+    @ivar del_count        number of called deletions
+    @ivar heterozygous     number of heterozygous variants
+    @ivar homozygous       number of homozygous variants
+    @ivar ins_count        number of called insertions
+    @ivar num_missense     number of missense variants
+    @ivar num_sense        number of sense variants
+    @ivar snv_count        number of SNVs
+    @ivar transitions      number of transitions
+    @ivar transversions     number of transversions
+    """
+
+    def __init__(self):
+        self.file_name = None
+        self.snv_count = 0
+        self.no_dbsnp_count = 0
+        self.dbsnp_count = 0
+        self.dbsnp_rare_count = 0
+        self.del_count = 0
+        self.heterozygous = 0
+        self.homozygous = 0
+        self.ins_count = 0
+        self.num_missense = 0
+        self.num_sense = 0
+        self.snv_count = 0
+        self.transitions = 0
+        self.transversions = 0
+
+    @classmethod
+    def generateForFile(self, vcf_file):
+        return VcfStatsGenerator(vcf_file).run()
+    
+
 class TrafficLights(object):
     """Handler for traffic lights thresholds."""
 
     def __init__(self, args):
         self.args = args
         self.thresholds = {
+            'aligned_reads': {'green': args.aligned_reads_green,
+                              'yellow': args.aligned_reads_yellow},
             'mean_coverage': {'green': args.mean_coverage_green,
                               'yellow': args.mean_coverage_yellow},
             'target_coverage_10x': {'green': args.target_coverage_10x_green,
@@ -159,7 +358,21 @@ class TrafficLights(object):
                                     'yellow': args.target_coverage_20x_yellow},
             }
 
-    def colorFor(self, category, value):
+    def colorPassFor(self, value):
+        """Return color for pass, warn, and fail."""
+        mapping = {'pass': 'green',
+                   'warn': 'yellow',
+                   'fail': 'red'}
+        return mapping.get(value, 'black')
+
+    def colorPassFunc(self):
+        """Return a function that returns the color for pass, warn, fail."""
+        def result(value):
+            return self.colorPassFor(value)
+        return result
+
+    def colorThreshFor(self, category, value):
+        """Return color for value and category, determined by threshold."""
         if not self.thresholds.get(category):
             return 'unknown category %s' % category
         if not self.thresholds[category].get('green'):
@@ -173,53 +386,83 @@ class TrafficLights(object):
         else:
             return 'red'
 
-    def colorFunc(self):
-        """Return a function that returns the color for a value in a category."""
+    def colorThreshFunc(self):
+        """Return a function that returns the color for a value in a category, by threshold."""
         def result(category, value):
-            return self.colorFor(category, value)
+            return self.colorThreshFor(category, value)
         return result
 
     @classmethod
     def addArguments(self, parser):
         """Add arguments to parser."""
-        parser.add_argument('--mean-coverage-green', type=float, default=0.85,
-                            help='lower bound for green color on mean coverage')
-        parser.add_argument('--mean-coverage-yellow', type=float, default=0.70,
-                            help='lower bound for green color on mean coverage')
+        group = parser.add_argument_group('quality thresholds')
+        group.add_argument('--mean-coverage-green', type=float, default=0.85,
+                           help='lower bound for green color on mean coverage')
+        group.add_argument('--mean-coverage-yellow', type=float, default=0.70,
+                           help='lower bound for green color on mean coverage')
 
-        parser.add_argument('--target-coverage-10x-green', type=float, default=0.85,
-                            help='lower bound for green color on 10x target coverage')
-        parser.add_argument('--target-coverage-10x-yellow', type=float, default=0.80,
-                            help='lower bound for green color on 10x target coverage')
+        group.add_argument('--target-coverage-10x-green', type=float, default=0.85,
+                           help='lower bound for green color on 10x target coverage')
+        group.add_argument('--target-coverage-10x-yellow', type=float, default=0.80,
+                           help='lower bound for green color on 10x target coverage')
 
-        parser.add_argument('--target-coverage-20x-green', type=float, default=0.85,
-                            help='lower bound for green color on 20x target coverage')
-        parser.add_argument('--target-coverage-20x-yellow', type=float, default=0.80,
-                            help='lower bound for green color on 20x target coverage')
+        group.add_argument('--target-coverage-20x-green', type=float, default=0.85,
+                           help='lower bound for green color on 20x target coverage')
+        group.add_argument('--target-coverage-20x-yellow', type=float, default=0.80,
+                           help='lower bound for green color on 20x target coverage')
+
+        group.add_argument('--aligned-reads-green', type=float, default=0.97,
+                           help='lower bound for green color on aligned reads percentage')
+        group.add_argument('--aligned-reads-yellow', type=float, default=0.90,
+                           help='lower bound for yellow color on aligned reads percentage')
 
 
-def renderReport(args, traffic_lights, sta):
+def renderReport(args, traffic_lights, sta, fastqcs, vcf_stats):
     """Render reports as configured in args from the collected statistics."""
+    def formatNumber(num):
+        import locale
+        locale.setlocale(locale.LC_ALL, 'en_US.utf-8')
+        return locale.format('%d', num, 1)
     # load HTML template file
     html = pkg_resources.resource_string('cbpipeline', 'qc_report.html')
     # render template, logic is there
-    tpl = jinja2.Template(html)
-    args.out_html.write(tpl.render(sta=sta, color=traffic_lights.colorFunc()))
+    env = jinja2.Environment(loader=jinja2.PackageLoader('cbpipeline', '.'))
+    env.filters['format_number'] = formatNumber
+    env.filters['basename'] = os.path.basename
+    tpl = env.get_template('qc_report.html')
+    args.out_html.write(tpl.render(sta=sta,
+                                   fastqcs=fastqcs,
+                                   vcf_stats=vcf_stats,
+                                   color_pass=traffic_lights.colorPassFunc(),
+                                   color_thresh=traffic_lights.colorThreshFunc(),
+                                   black_and_white=args.black_and_white,
+                               ))
 
 
 def main():
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('--sta-file', type=argparse.FileType('rb'),
-                        help='Picard statistics file', required=True)
-    parser.add_argument('--out-html', type=argparse.FileType('wb'),
-                        help='output HTML file', required=True)
+    parser.add_argument('--black-and-white', action='store_true', default=False,
+                        help='do not use colors in output')
+
+    file_group = parser.add_argument_group('input/output paths')
+    file_group.add_argument('--sta-file', type=argparse.FileType('rb'),
+                            help='Picard statistics file', required=True)
+    file_group.add_argument('--fastqc-zip', type=str, required=True,
+                            action='append', default=[], dest='fastqc_zips',
+                            help='path to FASTQC zip file')
+    file_group.add_argument('--vcf-file', type=argparse.FileType('rb'), required=True,
+                            help='path to VCF file to analyze')
+    file_group.add_argument('--out-html', type=argparse.FileType('wb'),
+                            help='output HTML file', required=True)
 
     TrafficLights.addArguments(parser)
 
     args = parser.parse_args()
     sta = AlignmentStatsFile.loadFromFile(args.sta_file)
+    fastqcs = [FastQCReport.loadFromFile(f) for f in args.fastqc_zips]
+    vcf_stats = VcfStats.generateForFile(args.vcf_file)
 
-    renderReport(args, TrafficLights(args), sta)
+    renderReport(args, TrafficLights(args), sta, fastqcs, vcf_stats)
 
 
 if __name__ == '__main__':
